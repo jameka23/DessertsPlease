911f02e8419286298f4738eddf20a890109b155d
Jameka Echols <jameka.echols@gmail.com>
2024-08-28 00:23:52 -0400

    Initial Commit

---
+++ DessertsPlease/DessertsPleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsPleaseApp.swift
+//  DessertsPlease
+//
+//  Created by Jameka Echols on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertsPleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
